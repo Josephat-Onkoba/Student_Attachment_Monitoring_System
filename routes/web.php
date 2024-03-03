5bd420b0e6ba75c9c7ee239584d0aa02abafcc87
Josephat-Onkoba <josephatonkoba30@gmail.com>
2024-03-04 00:10:40 +0300

    Login and Sign up pages

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/registration', function () {
+    return view('registration');
 });

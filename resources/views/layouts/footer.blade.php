03d69b4c4ebdcaf27ac0dd5765f63ac5cf8a6107
Josephat-Onkoba <josephatonkoba30@gmail.com>
2024-05-07 15:14:30 +0300

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,8 +6,3 @@
           <b>Version</b>  1.0.0
       </div>
   </footer>
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-      <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->

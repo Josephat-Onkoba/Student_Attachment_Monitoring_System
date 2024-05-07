78db4bd32698ff9b8353246203df94fbc46d5e89
Josephat-Onkoba <josephatonkoba30@gmail.com>
2024-05-07 15:13:44 +0300

    FOOTER

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-      <strong>Copyright &copy; 2014-2021 <a href="http://127.0.0.1:8000">Student Attachment monitoring System</a>.</strong>
+      <strong>Copyright &copy; 2024 <a href="http://127.0.0.1:8000">Student Attachment monitoring System</a>.</strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
           <b>Version</b>  1.0.0
